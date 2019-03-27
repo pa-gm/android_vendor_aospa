f17966ca7aa83e0bcd2fcc7aaab2516fd96cfd3d
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2019-03-26 21:44:19 -0400

    overlay: Add Notch Bar Killer
     * This overlay gets rid of the black bar in most apps/games
       and lets the screen fill past the notch.
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -17,6 +17,7 @@ PRODUCT_PACKAGES += \
     AOSPABlackThemeOverlay \
     AOSPAFrameworksOverlay \
     AOSPAImmersiveNavigationOverlay \
+    AOSPANotchBarKillerOverlay \
     AOSPASettingsOverlay \
     AOSPASettingsProviderOverlay \
     AOSPASystemUIOverlay \
