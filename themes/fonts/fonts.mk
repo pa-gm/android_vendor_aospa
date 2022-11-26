4ff94336c30d91cefa0eb186b1ebc78410ee23ef
Richard Raya <rdxzv.dev@gmail.com>
2022-11-26 17:21:36 -0300

    fonts: Import OnePlus Slate font
    Change-Id: I71c6a76d7458ef0b903af7799f7af2180b2c34fd
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- themes/fonts/fonts.mk
+++ themes/fonts/fonts.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
     FontManropeOverlay \
     FontMiLanProOverlay \
     FontOnePlusSansOverlay \
+    FontOnePlusSlateOverlay \
     FontOppoSansOverlay \
     FontRobotoOverlay \
     FontUrbanistOverlay
